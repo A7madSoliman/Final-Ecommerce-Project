ad5700b12c7dd3043c3320772f0a2d513aafbf2f
Ahmed Soliman <ahmadsoliman283@gmail.com>
2026-01-10 17:03:17 +0200

    Edited on scroll to top

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import ScrollToTop from "../../Helper/ScrollToTop";
+import OnTop from "../../Helper/OnTop";
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <OnTop />
       <Navbar />
 
       <main role="main" className="flex-1 pt-16">
